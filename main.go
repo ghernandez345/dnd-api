4c27ed968d9e8f5e45164a69209653b5d7cec618
Gabriel Hernandez <ghernandez345@gmail.com>
2025-07-27 20:58:42 +0100

    add logging to all endpoints

--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"log/slog"
 )
 
-func testHandler(w http.ResponseWriter, req *http.Request) {
-	slog.Info("request", "method", req.Method, "url", req.URL)
-	fmt.Fprint(w, "Hellow World!")
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		slog.Info("request", "method", req.Method, "url", req.URL)
+		next.ServeHTTP(w, req)
+	})
 }
+
+func testHandler(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+
+func test2Handler(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Hello two!"))
+}
+
 
 func main () {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", testHandler)
+	mux.HandleFunc("/test2", test2Handler)
 
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":8080", loggingMiddleware(mux))
 }
