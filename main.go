b067a1e32e2e8002e5cf90f346b2617ef8d18364
Gabriel Hernandez <ghernandez345@gmail.com>
2025-08-02 13:29:15 +0100

    return the correct json obj for class api request

--- main.go
+++ main.go
@@ -1,26 +1,64 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
-	"unicode/utf8"
+	"strings"
 )
 
+type class struct {
+	Index string `json:"index"`
+}
+
+func getVal(id string, jsonBlob []byte) (*class, error) {
+	var classes []class
+	json.Unmarshal(jsonBlob, &classes)
+	if classes == nil {
+		return nil, errors.New("no json data")
+	}
+
+	fmt.Printf("%+v", classes)
+
+	for _, val := range classes {
+		fmt.Println(val.Index)
+		if val.Index == id {
+			return &val, nil
+		}
+	}
+
+	return nil, errors.New("id not found")
+}
+
 func capitalize(s string) string {
-	r, _ := utf8.DecodeRuneInString(s)
+	if len(s) == 0 {
+		return s
+	}
+	return strings.ToUpper(s[:1]) + s[1:]
 }
 
 func classHandler(w http.ResponseWriter, req *http.Request) {
 	// read file
-	bytes, err := os.ReadFile(fmt.Sprintf("data/%s/5e-SRD-%s", req.PathValue("year"), capitalize(req.PathValue("id"))))
+	bytes, err := os.ReadFile(fmt.Sprintf("data/%s/5e-SRD-Classes.json", req.PathValue("year")))
 	if err != nil {
 		w.Write([]byte(err.Error()))
 		return
 	}
+	class, err := getVal(req.PathValue("id"), bytes)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	classBytes, err := json.Marshal(class)
+	if err != nil {
+		w.Write([]byte("error marshalling class"))
+		return
+	}
 
-	w.Write(bytes)
+	w.Write(classBytes)
 	// get class id from req path and get obj val by this key
 
 	// create struct and add it to w writer as json
